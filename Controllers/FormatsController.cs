80c4d9adfa3c6b77c52805671b231a6ac9123368
Ville-Pekka Hekkanen <ville-pekka.hekkanen@student.careeria.fi>
2023-06-15 15:06:50 +0300

    FormatsController created + getAll.

--- Controllers/FormatsController.cs
+++ Controllers/FormatsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MtGdbWebAPIbackend.Models;
 
 namespace MtGdbWebAPIbackend.Controllers
@@ -12,5 +13,27 @@ namespace MtGdbWebAPIbackend.Controllers
 
         // Depency Injection -tyyli
         //private readonly MtGdbContext db;
+
+        public FormatsController(MtGdbContext dbparam)
+        {
+            db = dbparam;
+        }
+
+        // Hakee kaikki deckit
+        [HttpGet]
+        [Route("")]
+        public List<Format> GetAll()
+        {
+            List<Format> formats = db.Formats
+                //.Include(cmdr => cmdr.Commanders)
+                //.Include(comp => comp.Companions)
+                //.Include(md => md.MainDecks)
+                //.Include(mb => mb.Maybeboards)
+                //.Include(sb => sb.Sideboards)
+                //.Include(t => t.Tokens)
+                .ToList();
+
+            return formats;
+        }
     }
 }
